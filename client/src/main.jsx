e012fab77a47d5c426a9ae530a3b7094c0d2227f
singhalgunjan5 <78017706+singhalgunjan5@users.noreply.github.com>
2024-02-13 00:40:15 +0530

    Remove all the sensitive information as i got to know i accidentally added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import RightSideProvider from './components/RightSideContext.jsx';
-const sensitiveInformation2 = "My_Sensitive_Information2" //Doing this to solve the issue of having sensitive information in previous commits
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
