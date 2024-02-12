edd5c5fef95da5a7db6e6bc7bdd65a51bc49132a
singhalgunjan5 <78017706+singhalgunjan5@users.noreply.github.com>
2024-02-13 00:35:50 +0530

    Add_sensitive_info2 (For Learning Purpose)

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import RightSideProvider from './components/RightSideContext.jsx';
+const sensitiveInformation2 = "My_Sensitive_Information2" //Doing this to solve the issue of having sensitive information in previous commits
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
