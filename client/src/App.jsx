841d498f515e004825279607e96c8d4f74d99d18
singhalgunjan5 <78017706+singhalgunjan5@users.noreply.github.com>
2024-02-13 00:35:11 +0530

    Add_sensitive_info1 (For Learning Purpose)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import './App.css';
 import Logo from './components/Logo';
 import LeftSide from './components/LeftSide';
 import RightSide from './components/RightSideContext';
-
+const sensitiveInformation = "My_Sensitive_Information" //Doing this to solve the issue of having sensitive information in previous commits
 function App() {
   return (
     <div className="app-container">
